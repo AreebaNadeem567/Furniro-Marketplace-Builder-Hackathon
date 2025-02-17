3ae8f40a1fdb53a6d866932aba5ad718d2a84e85
AREEBA NADEEM <AreebaNadeem567>
2025-02-17 16:41:53 +0500

    first commit

--- next.config.js
+++ next.config.js
@@ -1,25 +1,38 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     reactStrictMode: true, // Strict mode React ke liye enable karta hai
+//     images: {
+//       domains: ["cdn.sanity.io"], // Sanity ke image domain ko allowed list mein add karte hain
+//     },
+//   };
+  
+//   module.exports = nextConfig;
+
+
+
+
+
+
+
+
+
+
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true, // Strict mode React ke liye enable karta hai
-    images: {
-      domains: ["cdn.sanity.io"], // Sanity ke image domain ko allowed list mein add karte hain
-    },
-  };
-  
-  module.exports = nextConfig;
+  reactStrictMode: true, // Strict mode React ke liye enable karta hai
+  images: {
+    domains: ["cdn.sanity.io"], // Sanity ke image domain ko allowed list mein add karte hain
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, path: false };
+    return config;
+  },
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
+module.exports = nextConfig;
 
 
 
